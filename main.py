f638478cc27bb3b6f7c39bf059e21b8bc7ee8128
Gwyn Hannay <gwyn@hannay.id.au>
2022-06-06 10:02:22 +0800

    get diffs and create files

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import utils.docx_parser as docx
 import utils.data_cleaner as dc
+import difflib
+import sys
 
 def main():
     orig_doc = docx.open_document('hansard/The Constitution - as made version.docx')
     changed_doc = docx.open_document('hansard/C2021Q00024XN01.docx')
     orig_text = docx.get_document_text(orig_doc)
+    changed_text = docx.get_document_text(changed_doc)
 
-    i = 0
-    for line in orig_text:
-        i = i + 1
-        new_line = dc.remove_whitespace(line)
-        if new_line.strip():
-            print(new_line)
-        if i > 500:
-            break
+    # delta = difflib.HtmlDiff().make_table(orig_text, changed_text, 'Original Constitution', 'Constitution Changes')
+    # with open('table.html', 'w') as f:
+    #     f.write(delta)
+
+    delta = difflib.unified_diff(orig_text, changed_text, 'Original Constitution', 'Constitution Changes')
+    with open('changes.txt', 'w') as f:
+        for line in delta:
+            f.write(line)
+
+    # i = 0
+    # for line in orig_text:
+    #     i = i + 1
+    #     new_line = dc.remove_whitespace(line)
+    #     if new_line.strip():
+    #         print(new_line)
+    #     if i > 500:
+    #         break
 
 
 
