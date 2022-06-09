d398b5d5e6db31ee41191c029a2a5ad12329eb10
Gwyn Hannay <gwyn@hannay.id.au>
2022-06-09 10:14:02 +0800

    generate diff html files

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import utils.docx_parser as docx
-import utils.data_cleaner as dc
 import utils.legislation_crawler as crawler
 import utils.diff_builder as diff
-import difflib
 
 
 document_filepath = 'docs'
@@ -25,34 +22,8 @@ def main():
             previous_document = document
             continue
 
-        diff.generate_diff_page(document_order[previous_document], document_order[document])
+        diff.generate_diff_page(document_order[previous_document], document_order[document], document_filepath)
         previous_document = document
-
-
-def build_diff():
-    orig_doc = docx.open_document('hansard/The Constitution - as made version.docx')
-    changed_doc = docx.open_document('hansard/C2021Q00024XN01.docx')
-    orig_text = docx.get_document_text(orig_doc)
-    changed_text = docx.get_document_text(changed_doc)
-
-    # delta = difflib.HtmlDiff().make_table(orig_text, changed_text, 'Original Constitution', 'Constitution Changes')
-    # with open('table.html', 'w') as f:
-    #     f.write(delta)
-
-    delta = difflib.unified_diff(orig_text, changed_text, 'Original Constitution', 'Constitution Changes')
-    with open('changes.txt', 'w') as f:
-        for line in delta:
-            f.write(line)
-
-    # i = 0
-    # for line in orig_text:
-    #     i = i + 1
-    #     new_line = dc.remove_whitespace(line)
-    #     if new_line.strip():
-    #         print(new_line)
-    #     if i > 500:
-    #         break
-
 
 
 if __name__ == "__main__":
--- utils/diff_builder.py
+++ utils/diff_builder.py
@@ -1,8 +1,20 @@
+import utils.docx_parser as docx
 import difflib
+import os
 
 
-def generate_diff_page(old_document, new_document):
-    print('Old')
-    print(old_document['Filename'])
-    print('New')
-    print(new_document['Filename'])
+diff_dir = 'changes'
+
+def generate_diff_page(old_document, new_document, filepath):
+    orig_doc = docx.open_document(os.path.join(filepath, old_document['Filename']))
+    changed_doc = docx.open_document(os.path.join(filepath, new_document['Filename']))
+    orig_text = docx.get_document_text(orig_doc)
+    changed_text = docx.get_document_text(changed_doc)
+
+    old_document_name = ''.join([old_document['Title'], ', Comp: ', old_document['Comp No.']])
+    new_document_name = ''.join([new_document['Title'], ', Comp: ', new_document['Comp No.']])
+
+    delta = difflib.HtmlDiff().make_file(orig_text, changed_text, old_document_name, new_document_name)
+    with open(os.path.join(diff_dir, ''.join([new_document['RegisterId'], '.html'])), 'w') as f:
+        for line in delta:
+            f.write(line)
