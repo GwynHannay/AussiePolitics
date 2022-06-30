358dc5d4bbc07ed81e911d6a3f39b9d5b01a160e
Gwyn Hannay <gwyn@hannay.id.au>
2022-07-01 07:23:27 +0800

    lower version of cryptography on macos

--- legislation/legislation/spiders/acts.py
+++ legislation/legislation/spiders/acts.py
@@ -4,4 +4,9 @@ class ActsSpider(scrapy.Spider):
     name = "acts"
 
     def start_requests(self):
-        urls = ['https://www.legislation.gov.au/Browse/ByTitle/Acts/InForce/0/0/Principal']
+        url = 'https://www.legislation.gov.au/Series/C2004A03898'
+        yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        print('some text')
+        print(response.css('title'))
