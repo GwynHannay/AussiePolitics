ad24588f0d86351384636847aab3448ee601c446
Gwyn Hannay <gwyn@hannay.id.au>
2022-07-02 09:07:45 +0800

    clean up

--- utils/common.py
+++ utils/common.py
@@ -17,6 +17,17 @@ def build_url(base: str, part: str, prefix=None, suffix=None):
     return complete_url
 
 
+def build_url_from_config(common_config: dict, crawl_config: dict, type: str):
+    base_url = common_config['base_url']
+    part = crawl_config['landing_url']
+    type_entry = ''.join([type, '_url'])
+    prefix = common_config[type_entry].get('prefix')
+    suffix = common_config[type_entry].get('suffix')
+
+    complete_url = build_url(base_url, part, prefix, suffix)
+    return complete_url
+
+
 def get_soup(url: str):
     response = urlopen(url)
     soup = BeautifulSoup(response, "html.parser")
--- utils/series.py
+++ utils/series.py
@@ -7,24 +7,8 @@ def main(section: str, common_config: dict, crawl_config: dict):
         print(index_urls)
 
 
-def get_url_from_parts(common_config: dict, crawl_config: dict, type: str):
-    base_url = common_config['base_url']
-    part = crawl_config['landing_url']
-    type_entry = ''.join([type, '_url'])
-    prefix = common_config[type_entry].get('prefix')
-    suffix = common_config[type_entry].get('suffix')
-
-    complete_url = get_url(base_url, part, prefix, suffix)
-    return complete_url
-
-
-def get_url(base_url, part, prefix=None, suffix=None):
-    complete_url = common.build_url(base_url, part, prefix, suffix)
-    return complete_url
-
-
 def get_indexes(common_config: dict, crawl_config: dict):
-    landing_page_link = get_url_from_parts(common_config, crawl_config, type='index')
+    landing_page_link = common.build_url_from_config(common_config, crawl_config, type='index')
     landing_page_contents = common.get_soup(landing_page_link)
     landing_elements = {
         'type': 'a',
@@ -35,6 +19,6 @@ def get_indexes(common_config: dict, crawl_config: dict):
     raw_links = common.get_soup_elements(landing_page_contents, landing_elements)
     index_urls = []
     for link in raw_links:
-        index_urls.append(get_url(landing_page_link, link))
+        index_urls.append(common.build_url(landing_page_link, link))
     
     return index_urls
